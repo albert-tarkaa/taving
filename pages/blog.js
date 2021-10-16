454ecb35697f9179975dd2bda8499f1225e330be
Albert Tarkaa <tarkaa.albert@gmail.com>
2021-10-16 12:36:26 +0100

    escaped double quotes and added link to temp hosting

--- pages/blog.js
+++ pages/blog.js
@@ -8,12 +8,12 @@ const Blog = () => {
       <div className='container'>
         <main className='main'>
           <blockquote className='description'>
-            “I think human consciousness is a tragic misstep in human evolution. We became too self aware; nature
+            {`“I think human consciousness is a tragic misstep in human evolution. We became too self aware; nature
             created an aspect of nature separate from itself. We are creatures that should not exist by natural law. We
             are things that labor under the illusion of having a self, a secretion of sensory experience and feeling,
             programmed with total assurance that we are each somebody, when in fact everybody’s nobody. I think the
             honorable thing for our species to do is deny our programming, stop reproducing, walk hand in hand into
-            extinction, one last midnight, brothers and sisters opting out of a raw deal.”
+            extinction, one last midnight, brothers and sisters opting out of a raw deal.”`}
           </blockquote>
           <cite>― Rustin Cohle, True Detective</cite>
         </main>
--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ export default function Home() {
       <div className='container'>
         <main className='main'>
           <blockquote className='description'>
-            "I wish it need not have happened in my time," said Frodo. "So do I," said Gandalf, "and so do all who live
+           { `"I wish it need not have happened in my time," said Frodo. "So do I," said Gandalf, "and so do all who live
             to see such times. But that is not for them to decide. All we have to decide is what to do with the time
-            that is given us.”
+            that is given us.”`}
           </blockquote>
           <cite>― J.R.R. Tolkien, The Fellowship of the Ring</cite>
         </main>
