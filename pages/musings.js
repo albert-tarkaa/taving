e863c9bb6c092bf53d5940224e8e2c8f9b83c27e
albert-tarkaa <tarkaa.albert@gmail.com>
2023-03-13 23:46:58 +0100

    setup datocms for musings

--- pages/musings.js
+++ pages/musings.js
@@ -1,8 +1,14 @@
-import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { GraphQLClient } from 'graphql-request';
+import parse from 'html-react-parser';
+import * as dayjs from 'dayjs';
+var localizedFormat = require('dayjs/plugin/localizedFormat');
+dayjs.extend(localizedFormat)
 
-const Musings = () =>
+
+const Musings = ({ data }) =>
 {
+    console.log(data)
     return (
         <>
             <Header title='Musings - Poetry and stuff' musings='musings' />
@@ -10,24 +16,13 @@ const Musings = () =>
             <main className='main'>
                 <div className='description'>
                     <h2>The uncertainties and conversations with fate</h2>
-                    <p>#</p>
-                    <p>
-                        
-                        I worry. I dread days that make me think about the many uncertainties that reach out, the real ones and the thousand and a few that crawl from my mind, imagined in more detail than the scenes from the Silmarillion.
-
-                        I see so many wistful smiles, they mirror most moments, even on days when I do not choose to be lucid. The spires sticking out used to be my favorite place, it housed the calm of the shire, the lush grass and the cold mountain air that sweeps in, the grandeur of the halls of the Elfen folk, the inviting pews, solid and unyielding yet welcoming, maybe because it reminds me of wholeness, family, childhood, and its apparent lack of bother. It is a cocoon and maybe I can nest in here if the bishop will let me, but, I fear I will be lost in the past, in the memories of new clothes, movies, and delicacies and the car rides home after Sunday Masses.
-
-                        These days, these days all I wish for is the strength to overcome the bleakness
-                    </p>
-                    <p>#</p>
-                    <p>
-                        I don&apos;t think I would like it in the ground
-                        The earth gives but it also takes more
-                        Always taking, stoic, and entitled
-                        I would love to feel the wind as my ashes fly away
-                        Enjoying the freedom I so desperately craved
-                        It would be beautiful, one last time
-</p>
+                    {data.map(musing => (
+                        <>
+                            <p key={musing._firstPublishedAt}>#</p>
+                            <p style={{ whiteSpace: "pre-line" }}> {parse(`${musing.musings}`)}</p>
+                            <span className='publised'>pulished: {dayjs(musing._firstPublishedAt).format('LLLL')} </span>
+                        </>
+                    ))}                    
                 </div>
             </main>
             {/* <Footer /> */}
@@ -36,3 +31,34 @@ const Musings = () =>
 };
 
 export default Musings;
+
+export async function getStaticProps()
+{
+    const client = new GraphQLClient(process.env.NEXT_DATOCMS_API_URL, {
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+        },
+    });
+
+    const query = ` {
+  allTheUncertaintiesAndConversationsWithFates {
+    _firstPublishedAt
+    musings
+  }
+}`;
+
+
+    const data = await client.request(query);
+    if (!data)
+    {
+        return {
+            notFound: true,
+        };
+    }
+    return {
+        props: {
+            data: data.allTheUncertaintiesAndConversationsWithFates,
+        },
+    };
+}
+
