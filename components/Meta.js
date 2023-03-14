0cbacf19a99470110614ae7025fbfa7f03d163b4
albert-tarkaa <tarkaa.albert@gmail.com>
2023-03-14 11:54:11 +0100

    updated meta

--- components/Meta.js
+++ components/Meta.js
@@ -3,8 +3,8 @@ import Head from 'next/head';
 export const Meta = () => (
   <Head>
     <title>Albert Tarkaa</title>
-    <meta name='description' content='Check out my portfolio built with NextJS' />
-    <meta name='keywords' content='Albert Tarkaa NextJS ReactJS Portfolio' />
+    <meta name='description' content='Albert Tarkaa - Portfolio' />
+    <meta name='keywords' content='Albert Tarkaa, NextJS, Portfolio, Web development' />
     <meta name='author' content='Albert Tarkaa' />
     <meta property='og:type' content='website' />
     <meta name='google-site-verification' content='p5kfwl_U5MSgwzljChwGRFGQpvlZHgsyE7n1Lki_4XM' />
