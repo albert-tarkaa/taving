93a7fa06002a8b89afaf4d68b36fd23b4e22db8f
albert-tarkaa <tarkaa.albert@gmail.com>
2023-03-14 12:37:42 +0100

    updated meta

--- components/Meta.js
+++ components/Meta.js
@@ -1,23 +1,12 @@
 import Head from 'next/head';
 
 export const Meta = () => (
-  <>
-    <Head>
-      <title>Albert Tarkaa</title>
-      <meta name='description' content='Albert Tarkaa - Portfolio' />
-      <meta name='keywords' content='Albert Tarkaa, NextJS, Portfolio, Web development' />
-      <meta name='author' content='Albert Tarkaa' />
-      <meta property='og:type' content='website' />
-      <meta name='google-site-verification' content='p5kfwl_U5MSgwzljChwGRFGQpvlZHgsyE7n1Lki_4XM' />
-    </Head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7PBGFKC26K"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-7PBGFKC26K');
-    </script>
-  </>
-
+  <Head>
+    <title>Albert Tarkaa</title>
+    <meta name='description' content='Albert Tarkaa - Portfolio' />
+    <meta name='keywords' content='Albert Tarkaa, NextJS, Portfolio, Web development' />
+    <meta name='author' content='Albert Tarkaa' />
+    <meta property='og:type' content='website' />
+    <meta name='google-site-verification' content='p5kfwl_U5MSgwzljChwGRFGQpvlZHgsyE7n1Lki_4XM' />
+  </Head>
 );
